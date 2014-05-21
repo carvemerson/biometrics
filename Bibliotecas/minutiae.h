d59802906de72e6c419d73176acaa490f080c64d
Emerson <carvmerson@gmail.com>
2014-05-21 10:01:43 -0300

    Minutiae adicionado

---
+++ Bibliotecas/minutiae.h
@@ -0,0 +1,8 @@
+#include <cv.h>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+Mat minutiae(Mat &tmp);
--- make.sh
+++ make.sh
@@ -1,1 +1,1 @@
- g++ -ggdb `pkg-config --cflags opencv` Bibliotecas/equalization.h Bibliotecas/cvWiener2.h Bibliotecas/thinning.h equalization.cpp cvWiener2.cpp thinning.cpp main.cpp -o cv `pkg-config --libs opencv`
+ g++ -ggdb `pkg-config --cflags opencv` Bibliotecas/equalization.h Bibliotecas/cvWiener2.h Bibliotecas/thinning.h Bibliotecas/minutiae.h equalization.cpp cvWiener2.cpp thinning.cpp minutiae.cpp main.cpp -o cv `pkg-config --libs opencv`
